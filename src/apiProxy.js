4e019b21278c7340703db3d53943f9e047197742
Orr Steinberg <orr.sun10@gmail.com>
2021-05-03 22:39:48 +0300

    add POST and DELETE requests

--- src/apiProxy.js
+++ src/apiProxy.js
@@ -8,24 +8,65 @@ const request = axios.create({
   },
 });
 
-router.get("/:path", async (req, res) => {
+// Proxy incoming requests to YouTube's API
+
+router.get("/:path", async (req, res, next) => {
   try {
+    // Extract query params
     const path = req.params.path;
     const options = {
       params: req.query,
     };
 
+    // Add authorization headers if they're included in the req
     if (req.headers.authorization) {
       options.headers = {
         Authorization: req.headers.authorization,
       };
     }
 
+    // Return response from YouTube API
     const ytResponse = await request(path, options);
     return res.json(ytResponse.data);
   } catch (err) {
-    const error = err.response.data.error;
-    return res.status(error.code).json({ message: error.message });
+    next(err);
+  }
+});
+
+router.post("/:path", async (req, res, next) => {
+  try {
+    const path = req.params.path;
+    const options = {
+      method: "POST",
+      data: req.body,
+      params: req.query,
+      headers: {
+        Authorization: req.headers.authorization,
+      },
+    };
+
+    const ytResponse = await request(path, options);
+    return res.json(ytResponse.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:path", async (req, res, next) => {
+  try {
+    const path = req.params.path;
+    const options = {
+      method: "DELETE",
+      params: req.query,
+      headers: {
+        Authorization: req.headers.authorization,
+      },
+    };
+
+    const ytResponse = await request(path, options);
+    return res.json(ytResponse.data);
+  } catch (err) {
+    next(err);
   }
 });
 
