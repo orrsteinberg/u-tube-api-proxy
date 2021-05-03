29003340e2e92f3ade0b5015d74fbdd766b6e3f0
Orr Steinberg <orr.sun10@gmail.com>
2021-05-03 23:33:37 +0300

    add rate limiter

--- src/app.js
+++ src/app.js
@@ -9,9 +9,24 @@ const apiProxy = require("./apiProxy");
 
 const app = express();
 
+const rateLimit = require("express-rate-limit");
+
+// (for rate limiter) Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.set("trust proxy", 1);
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: {
+    message: "Too many requests, please try again later.",
+  },
+});
+
 // Middleware
 app.use(morgan("dev"));
 app.use(cors());
+app.use(limiter);
 app.use(bodyParser.json());
 
 // Router
